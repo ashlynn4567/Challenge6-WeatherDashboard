a128a26196c9bf157be10b811605dd3ce8626933
Ashley Smith <ashlynn4567@gmail.com>
2022-03-18 16:34:12 -0500

    testing api calls and building HTML skeleton

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=St.Louis&appid=758dc488dbd90e57e26ad181eba5db49";
+
+fetch(geoApiUrl);
+
+var weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=38.6529545&lon=-90.24111656024635&appid=758dc488dbd90e57e26ad181eba5db49";
+
+fetch(weatherApiUrl);
