cb32a27fe441718b0b949fe06999e15d5835b89d
Ashley's PC <ashlynn4567@gmail.com>
2022-03-20 14:27:39 -0500

    append temp, humidity, wind direction and speed, and uv index to current weather conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 // ----------------------------------------------------------------------------1. VARIABLES //
 // --------------------------------------------------------------------------2. SUBMIT FORM //
 // ---------------------------------------------------------------------------3. FETCH DATA //
+// -------------------------------------------------------------------------4. DISPLAY DATA //
 // ---------------------------------------------------?. FUNCTION CALLS AND EVENT LISTENERS //
 // ---------------------------------------------------------------------------------------- //
 
@@ -13,7 +14,11 @@
 var cityFormEl = document.getElementById("city-form");
 var cityInputEl = document.getElementById("city-input");
 var citySearchTerm = document.querySelectorAll(".city-name");
-var currentDateEl = document.querySelector(".date");
+var currentDateEl = document.querySelector(".current-date");
+var currentTempEl = document.querySelector(".current-temperature");
+var currentHumidityEl = document.querySelector(".current-humidity");
+var currentWindEl = document.querySelector(".current-wind-speed");
+var currentUVEl = document.querySelector(".current-uv-index");
 
 // ---------------------------------------------------------------------------END VARIABLES //
 
@@ -75,7 +80,7 @@ var fetchWeatherData = function(latitude, longitude) {
     var weatherApiUrl = 
         "https://api.openweathermap.org/data/2.5/onecall?lat=" 
         + latitude + "&lon=" + longitude +
-        "&appid=758dc488dbd90e57e26ad181eba5db49";
+        "&units=imperial&appid=758dc488dbd90e57e26ad181eba5db49";
    
     fetch(weatherApiUrl).then(function(response2) {
         response2.json().then(function(data) {
@@ -88,30 +93,49 @@ var fetchWeatherData = function(latitude, longitude) {
 // --------------------------------------------------------------------------END FETCH DATA //
 
 
+
+
+// 4. DISPLAY DATA------------------------------------------------------------------------- //
 var displayCityName = function(cityName) {
+    // append city name to headers on page
     citySearchTerm[0].innerText = cityName;
     citySearchTerm[1].innerText = cityName;
 };
 
+
 var displayCurrentWeather = function(data) {
-    // date
-    var unixDate = data.current.dt
-    // converting unix to millisecs
-    var date = new Date(unixDate * 1000);
+    // collect date data from api
+    var unixDate = data.current.dt;
+    // converting unix to millisecs and formatting date
+    var currentDate = (new Date(unixDate * 1000)).toDateString();
+    // append date to page
+    currentDateEl.innerText = currentDate;
 
-    console.log(date);
+    // append temperature to page
+    currentTempEl.innerText = data.current.temp + " °F";
+    
+    // append humidity to page
+    currentHumidityEl.innerText = data.current.humidity + " %";
 
-    currentDateEl.innerText = date;
+    // collect wind direction from api
+    var windDegrees = data.current.wind_deg;
+    // convert wind direction degrees to cardinal directions
+    var val = Math.floor((windDegrees / 22.5) + 0.5);
+    var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+    var currentWindDegrees = arr[(val % 16)];
+    // append wind speed and wind direction to page
+    currentWindEl.innerText = data.current.wind_speed + " mph " + currentWindDegrees;
 
-    // temperature
-    // humidity
-    // wind-speed
-    // uv-index
+    // append uv-index to page
+    currentUVEl.innerText = data.current.uvi;
 };
+
 
 var displayFiveDayWeather = function() {
     
 };
+// ------------------------------------------------------------------------END DISPLAY DATA //
+
 
 
 
